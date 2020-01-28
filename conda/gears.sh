1db40402a5c44ecf98d0fdd34ebcd14aac385c03
Johannes Blaschke <jpblaschke@lbl.gov>
2020-01-28 13:47:21 -0800

    work on module loading

--- conda/gears.sh
+++ conda/gears.sh
@@ -10,11 +10,7 @@ _module_get () {
 
     # ${mod_grep##*) } drop the ` N) ` part of the `module` output: drop
     # everything _before_ the `) ` character set
-    if [[ ${mod_grep##*) } = $@ ]]; then
-        return 0
-    else
-        return 1
-    fi
+    echo ${mod_grep##*) }
 }
 
 _module_get_name_str () { echo ${@%%/*}; }
--- conda/load_modules.sh
+++ conda/load_modules.sh
@@ -1,8 +1,33 @@
 #!/usr/bin/env bash
 
 
+# load helper functions _module_**
+source gears.sh
+
+
+# load site-specific variables: XTC_REQ_MODULES
 if [[ $NERSC_HOST = "cori" ]]; then
     source cori_deps.sh
 fi
 
 
+
+# load site-sepcific modules
+for mod in ${XTC_REQ_MODULES[@]}; do
+
+    # check if not already loaded
+    if ! _module_loaded $mod; then
+        # module base name and version
+        mod_bn=$(_module_get_name_str $mod)
+
+        # check if another version hasn't already been loaded
+        if _module_loaded $mod_bn; then
+            mod_existing=$(_module_get $mod_bn)
+            echo "Switching from $mod_existing to $mod"
+            module switch $mod_existing $mod
+        else
+            echo "Loading $mod"
+            module load $mod
+        fi
+    fi
+done
