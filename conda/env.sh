ea8ef8bf457dc175fc1f856c7bf9cd51d8e5606a
Johannes Blaschke <jpblaschke@lbl.gov>
2020-02-12 07:06:48 -0800

    add conda environment loader

---
+++ conda/env.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+source $(dirname $BASH_SOURCE)/../gears.sh
+source $(dirname $BASH_SOURCE)/../load_modules.sh
+
+
+# load site-specific variables: XTC_**
+if [[ $NERSC_HOST = "cori" ]]; then
+    source $(dirname $BASH_SOURCE)/../cori_deps.sh
+fi
+
+
+env_grep=$(conda env list | grep $XTC_CONDA_ENV)
+if [[ -z $env_grep ]]; then
+    echo "$XTC_CONDA_ENV does not exist... exiting"
+    exit
+fi
+
+if [[ $NERSC_HOST = "cori" ]]; then
+    source activate $XTC_CONDA_ENV
+else
+    conda activate $XTC_CONDA_ENV
+fi
