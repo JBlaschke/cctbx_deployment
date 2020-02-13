78f07b670e1925f64f9c3cea9c2850b586a211ae
Johannes Blaschke <jpblaschke@lbl.gov>
2020-02-12 20:49:00 -0800

    move boostrap into cctbx dir

--- pipelines/xtc_process/update_boostrap.sh
+++ pipelines/xtc_process/update_boostrap.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+# get the directory of this pipeline
+my_dir=$(dirname ${BASH_SOURCE[0]})
+pipeline_dir=$(readlink -f $my_dir)
+
+export CCTBX_PREFIX=$pipeline_dir/cctbx
+pushd $CCTBX_PREFIX
 wget "https://raw.githubusercontent.com/cctbx/cctbx_project/master/libtbx/auto_build/bootstrap.py"
+popd
