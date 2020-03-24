66beeaf44552c5051f10f3a3b4d4f45a4a6a9bb5
Johannes Blaschke <jpblaschke@lbl.gov>
2020-03-23 21:09:41 -0700

    deal with shifter/cori interactions

--- docker/cctbx-entrypoint.sh
+++ docker/cctbx-entrypoint.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-source /opt/pipelines/xtc_process/env.local
+# Because of the way that env.local (and its dependencies) are designed, we need
+# CWD to be /opt/pipelines/xtc_process <= might be fixed later
+pushd /opt/pipelines/xtc_process >> /dev/null
+
+# HACK: don't load the cori-specifics (the module system will interfere with
+# docker/shifter:
+__old_nersc_host=NERSC_HOST
+NERSC_HOST="docker"
+
+# Load cctbx enviromnet
+source env.local 
+
+# restore NERSC_HOST (might be used somewhere else)
+NESC_HOST=__old_nersc_host
+
+# go back
+popd >> /dev/null
+
 exec "$@"
