a333a3cc4750b671d0eeb78868e997abbc202987
Johannes Blaschke <jpblaschke@lbl.gov>
2020-07-16 18:11:29 -0400

    add summit specific vars

--- opt/env/vars.sh
+++ opt/env/vars.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
 
+
 export XTC_CONDA_ENV="xtc_base"
 
 export XTC_PYVER="3.6"
 
-export XTC_CONDA_REQ=(
-    "xtc_default.txt"
-    "xtc_lcls-ii.txt"
-)
 
-export XTC_CONDA_CH=(
-    "default"
-    "lcls-ii"
-)
+_hostname=$(hostname -f)
+if [[ ${_hostname#login*.} == "summit.olcf.ornl.gov" ]]; then
+    export XTC_CONDA_REQ=(
+        "xtc_default_notag.txt"
+        "xtc_lcls-ii_notag.txt"
+    )
+
+    export XTC_CONDA_CH=(
+        "default"
+        "lcls-ii"
+    )
+else
+    export XTC_CONDA_REQ=(
+        "xtc_default.txt"
+        "xtc_lcls-ii.txt"
+    )
+
+    export XTC_CONDA_CH=(
+        "default"
+        "lcls-ii"
+    )
+fi
 
 export XTC_PIP="xtc_pip.txt"
