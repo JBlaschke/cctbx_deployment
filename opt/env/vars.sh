3f1a7cb73acdb92c57e6d7250c1e726f48297ff4
Johannes Blaschke <jpblaschke@lbl.gov>
2020-07-19 15:00:34 -0700

    rename pkg lists

--- opt/env/vars.sh
+++ opt/env/vars.sh
@@ -9,24 +9,20 @@ export XTC_PYVER="3.6"
 _hostname=$(hostname -f)
 if [[ ${_hostname#login*.} == "summit.olcf.ornl.gov" ]]; then
     export XTC_CONDA_REQ=(
-        "xtc_default_notag.txt"
-        "xtc_lcls-ii_notag.txt"
-    )
-
-    export XTC_CONDA_CH=(
-        "default"
-        "lcls-ii"
+        "xtc_default_power9.txt"
+        "xtc_lcls-ii_power9.txt"
     )
 else
     export XTC_CONDA_REQ=(
-        "xtc_default.txt"
-        "xtc_lcls-ii.txt"
-    )
-
-    export XTC_CONDA_CH=(
-        "default"
-        "lcls-ii"
+        "xtc_default_x86_64.txt"
+        "xtc_lcls-ii_x86_64.txt"
     )
 fi
+
+
+export XTC_CONDA_CH=(
+    "default"
+    "lcls-ii"
+)
 
 export XTC_PIP="xtc_pip.txt"
