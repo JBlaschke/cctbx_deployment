0637477402b61052b7c8b6bf59d8007323a2e70a
Johannes Blaschke <jpblaschke@lbl.gov>
2020-08-20 17:20:23 -0700

    added platform-agnostic heuristics to mk_ls49

--- opt/cctbx/mk/mk_ls49.sh
+++ opt/cctbx/mk/mk_ls49.sh
@@ -7,6 +7,18 @@ alias this="readlink -f \$(dirname \${BASH_SOURCE[0]})"
 
 
 pushd $(this)/../build
-python ../modules/cctbx_project/libtbx/configure.py --enable_openmp_if_possible=True --enable_cuda LS49 prime iota --use_conda --use_environment_flags
+
+comp_str=
+if [[ -n "$CC" || -n "$CXX" ]]; then
+    comp_str=--compiler=conda
+fi
+
+env_str=
+if [[ -n "$CFLAGS" || -n "$CXXFLAGS" ]]; then
+    env_str=--use_environment_flags
+fi
+
+python ../modules/cctbx_project/libtbx/configure.py --enable_openmp_if_possible=True --enable_cuda LS49 prime iota --use_conda $comp_str $env_str
 ./bin/libtbx.scons -j16
+
 popd
